8b0b8f016c7051a2b0b383cdb76e57aab8988110
Andy-CSKim <47920625+Andy-CSKim@users.noreply.github.com>
2024-04-21 21:13:47 +0900

    add CORS

--- explain.py
+++ explain.py
@@ -2,7 +2,7 @@ from typing import Union, Annotated
 
 from fastapi import FastAPI, Header
 from pydantic import BaseModel
-
+from fastapi.middleware.cors import CORSMiddleware
 
 class Item(BaseModel):
     name: str
@@ -16,6 +16,16 @@ class User(BaseModel):
 
 app = FastAPI()
 
+origins = ["http://localhost:3000",
+           ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")  # /
 def read_root():
